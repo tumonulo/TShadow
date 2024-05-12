539da5e41cc909eb15d6f59ea72cf6a459aa5ac0
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-05-12 17:28:06 +0200

    a

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function Invite() {
     window.location.href = "https://discord.com/oauth2/authorize?client_id=1226276748677873675&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Fchannels%2F%40me%2F1226292854624751699&scope=bot";
 }
 function Commands() {
-    
+    window.location.href = "https://tumonulo.github.io/TShadow/commands";
 }
 function Support() {
     window.location.href = "https://discord.gg/FZYM3SEFb6";
