cfb476c8d8ccfcb3136dd59cb1216a83392c08f8
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-05-12 15:58:58 +0200

    m

---
+++ index.js
@@ -0,0 +1,23 @@
+function TShadow() {
+    if (!window.location.href === "https://tumonulo.github.io/TShadow/") {
+        window.location.href = "https://tumonulo.github.io/TShadow/";
+    }
+}
+function Invite() {
+    window.location.href = "https://discord.com/oauth2/authorize?client_id=1226276748677873675&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Fchannels%2F%40me%2F1226292854624751699&scope=bot";
+}
+function Commands() {
+    
+}
+function Support() {
+    window.location.href = "https://discord.gg/FZYM3SEFb6";
+}
+function Premium() {
+    window.location.href = "https://patreon.com/TShadowPatreons";
+}
+function Partners() {
+    
+}
+function Dashboard() {
+
+}
