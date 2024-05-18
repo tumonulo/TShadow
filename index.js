789b9525b6381cc82b526d4e74518c3cc6019d6e
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-05-18 17:17:03 +0200

    a

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
-const tshadow = document.getElementById("TShadow");
-const invite = document.getElementById("Invite");
-const commands = document.getElementById("Commands");
-const support = document.getElementById("Support");
-const premium = document.getElementById("Premium");
-const partners = document.getElementById("Partners");
-const dashboard = document.getElementById("Dashboard");
+const elementinvitethebot = document.getElementById("invitethebot");
+const elementsupportserver = document.getElementById("supportserver");
 
 function TShadow() {
     if (!window.location.href === "https://tumonulo.github.io/TShadow/") {
         window.location.href = "https://tumonulo.github.io/TShadow/";
     }
 }
-function Invite() {
-    //window.location.href = "https://discord.com/oauth2/authorize?client_id=1226276748677873675&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Fchannels%2F%40me%2F1226292854624751699&scope=bot";
+function invitethebot() {
+    window.location.href = "https://discord.com/oauth2/authorize?client_id=1226276748677873675&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Fchannels%2F%40me%2F1226292854624751699&scope=bot";
+}
+function supportserver() {
+    window.location.href = "https://discord.gg/HGZTRa3TMf";
 }
 function Dashboard() {
     window.location.href = "https://tumonulo.github.io/TShadow/dashboard";
 }
 
-tshadow.addEventListener("mouseenter", colorChangeFontLightBlue);
-tshadow.addEventListener("mouseleave", colorChangeFontWhite);
-invite.addEventListener("mouseenter", colorChangeFontLightBlue);
-invite.addEventListener("mouseleave", colorChangeFontWhite);
-commands.addEventListener("mouseenter", colorChangeFontLightBlue);
-commands.addEventListener("mouseleave", colorChangeFontWhite);
-support.addEventListener("mouseenter", colorChangeFontLightBlue);
-support.addEventListener("mouseleave", colorChangeFontWhite);
-premium.addEventListener("mouseenter", colorChangeFontLightBlue);
-premium.addEventListener("mouseleave", colorChangeFontWhite);
-partners.addEventListener("mouseenter", colorChangeFontLightBlue);
-partners.addEventListener("mouseleave", colorChangeFontWhite);
-dashboard.addEventListener("mouseenter", colorChangeBackgroundBlackBlue);
-dashboard.addEventListener("mouseleave", colorChangeBackgorundBlue);
+elementinvitethebot.addEventListener("mouseenter", elementinvitethebotenter);
+elementinvitethebot.addEventListener("mouseleave", elementinvitethebotleave);
+elementsupportserver.addEventListener("mouseenter", elementsupportserverenter);
+elementsupportserver.addEventListener("mouseleave", elementsupportserverleave);
 
-
-function colorChangeFontLightBlue(event) {
-    event.target.style.color = "rgb(104, 175, 250)";
+function elementinvitethebotenter() {
+    elementinvitethebot.style.backgroundColor = "white";
+    elementinvitethebot.style.color = "transparent";
 }
-function colorChangeFontWhite(event) {
-    event.target.style.color = "white";
+function elementinvitethebotleave() {
+    elementinvitethebot.style.backgroundColor = "rgb(2, 77, 151)";
+    elementinvitethebot.style.color = "white";
 }
-function colorChangeBackgroundBlackBlue(event) {
-    event.target.style.backgroundColor = "rgba(1, 66, 136, 0.4)";
+function elementsupportserverenter() {
+    elementsupportserver.style.backgroundColor = "white";
+    elementsupportserver.style.color = "rgb(2, 77, 151)";
 }
-function colorChangeBackgorundBlue(event) {
-    event.target.style.backgroundColor = "rgba(1, 52, 107, 0.4)";
+function elementsupportserverleave() {
+    elementsupportserver.style.backgroundColor = "transparent";
+    elementsupportserver.style.color = "white";
 }
