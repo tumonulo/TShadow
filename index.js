f42b134672e95304c29c17ecb6de3a8b8db1708d
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-05-18 16:33:57 +0200

    m

--- index.js
+++ index.js
@@ -14,16 +14,6 @@ function TShadow() {
 function Invite() {
     //window.location.href = "https://discord.com/oauth2/authorize?client_id=1226276748677873675&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Fchannels%2F%40me%2F1226292854624751699&scope=bot";
 }
-function Commands() {
-
-}
-function Support() {
-
-}
-function Premium() {
-}
-function Partners() {
-}
 function Dashboard() {
     window.location.href = "https://tumonulo.github.io/TShadow/dashboard";
 }
