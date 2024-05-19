faec33cf4487540b4ac3cf7f6c38f2833b838f45
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-05-19 18:59:50 +0200

    s

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function TShadow() {
     }
 }
 function invitethebot() {
-    window.location.href = "https://discord.com/oauth2/authorize?client_id=1226276748677873675&permissions=633318697467895&scope=bot";
+    window.location.href = "https://discord.com/oauth2/authorize?client_id=1226276748677873675&permissions=8&scope=bot";
 }
 function supportserver() {
     window.location.href = "https://discord.gg/HGZTRa3TMf";
