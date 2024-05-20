f16bf471a74b59c58874b2afcc84243f73274ca4
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-05-20 15:48:39 +0200

    a

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ function TShadow() {
         window.location.href = "https://tumonulo.github.io/TShadow/";
     }
 }
+function Dashboard() {
+    window.location.href = "https://tumonulo.github.io/TShadow/dashboard";
+}
+
 function invitethebot() {
     window.location.href = "https://discord.com/oauth2/authorize?client_id=1226276748677873675&permissions=8&scope=bot";
 }
 function supportserver() {
     window.location.href = "https://discord.gg/HGZTRa3TMf";
 }
+
+function invite2() {
+    window.location.href = "https://discord.com/oauth2/authorize?client_id=1226276748677873675&permissions=8&scope=bot/";
+}
 function commands2() {
     window.location.href = "https://tumonulo.github.io/TShadow/commands/";
-}
-function Dashboard() {
-    window.location.href = "https://tumonulo.github.io/TShadow/dashboard";
 }
